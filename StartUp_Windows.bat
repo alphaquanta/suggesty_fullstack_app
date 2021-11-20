639cdde9d075e5d8ac2d5f483d0071d849495b54
achias <ismail.bozkuf@gmail.com>
2021-11-20 15:06:07 +0300

    typo fix

--- StartUp_Windows.bat
+++ StartUp_Windows.bat
@@ -1,2 +1,2 @@
 cd suggesty_frontend_angular
-npm install & ng build suggestyangular --configuration=development & cd.. & cd.. & docker-compose -f .\docker-compose-django-angular.yml up --build -d & PAUSE
+npm install & ng build suggestyangular --configuration=development & cd.. & docker-compose -f .\docker-compose-django-angular.yml up --build -d & PAUSE
